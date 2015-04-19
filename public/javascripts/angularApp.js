8ce6b3f8f649790bfba9178a8575ae5c22aebed7
Bryan Cho <bryanc208@gmail.com>
2015-04-19 13:27:33 -0400

    Got the basic functionality working!

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,30 +1,34 @@
-var app = angular.module('myBlog', []);
+var app = angular.module("myBlog", ['ui.router']);
 
-app.controller("MainCtrl", [
-'$scope',
-'posts',
-function($scope, posts) {                            
-    $scope.posts = posts.posts;
-    $scope.test = "Hello world!";
-    $scope.posts = [];
-    $scope.addPost = function(){
-        if(!$scope.title || $scope.title === ''){
-            alert("Enter a valid title!");
-            return;
-        }
-        $scope.posts.push({
-            title: $scope.title,
-            body: $scope.body,
-            likes: 0
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider){
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainCtrl',
+            resolve: {
+                postPromise: ['posts', function(posts){
+                    var allPosts = posts.getAllPosts();
+                    return allPosts;
+                }]
+            }
+        })
+        .state('posts', {
+            url: '/posts/:id',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl',
+            resolve: {
+                post: ['$stateParams', 'posts', function($stateParams, posts){
+                    var singlePost = posts.getSinglePost($stateParams.id);
+                    return singlePost;    
+                }]
+            }
         });
-        $scope.title = '';
-        $scope.body = '';
-    };
-    
-    $scope.likePost = function(post){
-        post.likes++;
-    }
-}   
+    $urlRouterProvider.otherwise('home');
+}
 ]);
 
 app.factory('posts', ['$http', function($http){
@@ -32,11 +36,81 @@ app.factory('posts', ['$http', function($http){
         posts: []
     };
     
-    postsObject.getAll = function(){
-        return $http.get('/posts').success(function(data){
-            angular.copy(data,postsObject.posts);
+    postsObject.getSinglePost = function(id){
+        return $http.get('/posts/' + id).then(function(res){
+            return res.data;
         });
     };
     
+    postsObject.getAllPosts = function(){
+        return $http.get('/posts').success(function(data){
+            angular.copy(data, postsObject.posts);
+        });
+    };
+    
+    postsObject.createNewPost = function(post){
+        return $http.post('/posts', post).success(function(data){
+            postsObject.posts.push(data);
+        });
+    };
+    
+    postsObject.incrementLikes = function(post){
+        return $http.put('/posts/' + post._id+'/likePost')
+            .success(function(data){
+                post.likes += 1;
+            });
+    };
+    
+    postsObject.postComment = function(id, comment){
+        return $http.post('/posts/' + id + '/comments', comment);
+    };
+    
     return postsObject;
-}])
+}]);
+
+app.controller('MainCtrl', [
+'$scope',
+'posts',
+function($scope, posts) {
+    $scope.posts = posts.posts;
+    $scope.addPost = function(){
+        if(!$scope.title || $scope.title === ''){
+            alert("Enter a valid title!");
+            return;
+        }
+        posts.createNewPost({
+            title: $scope.title,
+            body: $scope.body
+        });
+        $scope.title = '';
+        $scope.body = '';
+    };
+    
+    $scope.likePost = function(post){
+        posts.incrementLikes(post);
+    };
+}   
+]);
+
+
+app.controller('PostsCtrl', [
+'$scope',
+'posts',
+'post',
+function($scope, posts, post){
+    $scope.post = post;
+    
+    $scope.postComment = function(){
+        if($scope.body === '') {
+            return;
+        }
+        var comment = {
+            author: $scope.author,
+            body: $scope.body
+        }
+        posts.postComment(post._id, comment);
+        $scope.post.comments.push(comment);
+        $scope.body = '';
+    }
+}]);
+
